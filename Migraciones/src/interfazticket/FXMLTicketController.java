9c74fb765e2459ac0022af3f57c445f33c2a58f0
caloja <caloja@espol.edu.ec>
2019-11-15 23:17:20 -0500

    creacion de interfaz ticket

---
+++ Migraciones/src/interfazticket/FXMLTicketController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfazticket;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author CLOJA
+ */
+public class FXMLTicketController implements Initializable {
+
+    @FXML
+    private Label labelTurno;
+    @FXML
+    private Label labelTipoPersona;
+    @FXML
+    private Button btnTerceraEdad;
+    @FXML
+    private Button btnEspeciales;
+    @FXML
+    private Button btnNormal;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
