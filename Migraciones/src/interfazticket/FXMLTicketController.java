9cdab240d3f5df15272e4439a46ed33577361bdb
caloja <caloja@espol.edu.ec>
2019-11-23 14:51:16 -0500

    diseño tickets

--- Migraciones/src/interfazticket/FXMLTicketController.java
+++ Migraciones/src/interfazticket/FXMLTicketController.java
@@ -5,6 +5,7 @@
  */
 package interfazticket;
 
+import java.awt.Color;
 import modelo.AgenciaMigratoria;
 import modelo.Ticket;
 import java.net.URL;
@@ -17,6 +18,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 
 /**
  * FXML Controller class
@@ -30,13 +37,15 @@ public class FXMLTicketController implements Initializable {
     @FXML
     private Label labelTipoPersona;
     @FXML
-    private Button btnTerceraEdad;
-    @FXML
-    private Button btnEspeciales;
-    @FXML
-    private Button btnNormal;
-    @FXML
     private Label mostrarTurno;
+    @FXML
+    private ImageView discapacitados;
+    @FXML
+    private ImageView normal;
+    @FXML
+    private ImageView terceraEdad;
+    @FXML
+    private AnchorPane anchor;
 
     /**
      * Initializes the controller class.
@@ -45,25 +54,11 @@ public class FXMLTicketController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        anchor.setStyle("-fx-background-color: #ffffff");
     }    
 
-    @FXML
-    private void turnoD(ActionEvent event) {
-        asignarTurno(2);
-    }
-
-    @FXML
-    private void turnoT(ActionEvent event) {
-        asignarTurno(3);
-    }
-
-    @FXML
-    private void turnoU(ActionEvent event) {
-        asignarTurno(1);
-    }
-    private void asignarTurno(int tur){       
-        Ticket t = new Ticket(tur);
+    private void asignarTurno(int tur){
+        Ticket t= new Ticket(tur);
         AgenciaMigratoria.turnos.offer(t);
         mostrarTurno.setText("Su turno es: "+t.getId());
         new Thread (() -> {
@@ -77,5 +72,19 @@ public class FXMLTicketController implements Initializable {
                 Logger.getLogger(FXMLTicketController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }).start();
+    }
+
+    @FXML
+    private void turnoD(MouseEvent event) {
+        asignarTurno(2);
+    }
+
+    @FXML
+    private void turnoU(MouseEvent event) {
+        asignarTurno(1);
+    }
+    @FXML
+    private void turnoT(MouseEvent event) {
+        asignarTurno(3);
     }
 }
