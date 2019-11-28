15f9bca0a02654612a529a2b4e36f1158b39dc8e
caloja <caloja@espol.edu.ec>
2019-11-27 22:56:34 -0500

    Arreglo InterfazTurnos

--- Migraciones/src/interfazturnos/FXMLTurnosController.java
+++ Migraciones/src/interfazturnos/FXMLTurnosController.java
@@ -30,11 +30,20 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.util.Duration;
@@ -61,8 +70,6 @@ public class FXMLTurnosController implements Initializable {
     @FXML
     private ImageView publicidadIV;
     @FXML
-    private Label turnoOcupado;
-    @FXML
     private Label lblTurnos;
     @FXML
     private Label label1;
@@ -79,9 +86,19 @@ public class FXMLTurnosController implements Initializable {
     private static ArrayList<Label> labelsTurnosEspera;
     private static final PriorityQueue<Ticket> tickets=AgenciaMigratoria.turnos;
     private static PriorityQueue<Ticket> turnosEspera=new PriorityQueue<>((Ticket t1, Ticket t2) -> (t2.getPrioridad()-t1.getPrioridad()));
-    private static  final HashMap<Integer,Label>puestosTurnos=new HashMap<>();
+    private static  final HashMap<Integer,Text>puestosTurnos=new HashMap<>();
     @FXML
     private Label label7;
+    @FXML
+    private VBox principal;
+    @FXML
+    private Label horarioAtencion;
+    @FXML
+    private HBox contenedorHorario;
+    @FXML
+    private HBox contenedorTurnos;
+    @FXML
+    private HBox contenedorTiempo;
     /**
      * Initializes the controller class.
      */
@@ -89,6 +106,12 @@ public class FXMLTurnosController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        principal.setSpacing(5);
+        principal.setStyle("-fx-background-color: #FFFFFF;");
+        horarioAtencion.setStyle("-fx-background-color: #66B2FF;");
+       // contenedorHorario.setPadding(new Insets(5));
+        contenedorTurnos.setStyle("-fx-background-color: #66B2FF;");
+      //  contenedorTurnos.setPadding(new Insets(5));
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {        
             LocalTime currentTime = LocalTime.now();
             if(currentTime.getMinute() < 10){
@@ -100,10 +123,12 @@ public class FXMLTurnosController implements Initializable {
              new KeyFrame(Duration.seconds(1))
         );
         clock.setCycleCount(Animation.INDEFINITE);
-        clock.play();        
-           
-        publicidadIV.fitHeightProperty().bind(publicidad.heightProperty());
-        publicidadIV.fitWidthProperty().bind(publicidad.widthProperty());
+        clock.play();
+        contenedorTiempo.setStyle("-fx-background-color: #6666FF;");
+        tiempo.setTextFill(Color.WHITE);
+        tiempo.setFont(new Font(25));
+       // publicidadIV.fitHeightProperty().bind(publicidad.heightProperty());
+        //publicidadIV.fitWidthProperty().bind(publicidad.widthProperty());
         publicidadIV.setPreserveRatio(false);
         labelsTurnosEspera=  new ArrayList<>();
         labelsTurnosEspera.add(label1);
@@ -119,6 +144,7 @@ public class FXMLTurnosController implements Initializable {
             Logger.getLogger(FXMLTurnosController.class.getName()).log(Level.SEVERE, null, ex);
         }
         System.out.println(listaPublicidad);
+        crearLabelsPuestos();
         publicidadLoop();
         ticketSimulacion();
     }
@@ -186,7 +212,7 @@ public class FXMLTurnosController implements Initializable {
     
     public static void actualizarPuesto(Integer pos){
         Ticket t =turnosEspera.poll();
-        Label puesto=puestosTurnos.get(pos);
+        Text puesto=puestosTurnos.get(pos);
         puesto.setText(t.getId());
         Ticket t2=tickets.poll();
         turnosEspera.offer(t2);
@@ -205,12 +231,58 @@ public class FXMLTurnosController implements Initializable {
     
     public void crearLabelsPuestos(){
         int h=1;
+        Rectangle pues = new Rectangle(155,30);
+        pues.setFill(Color.rgb(102, 178, 255));
+        pues.setStroke(Color.rgb(102, 178, 255));
+        StackPane stack3 = new StackPane();
+        Text t3=new Text("PUESTO");
+        t3.setFont(new Font(25));
+        t3.setFill(Color.WHITE);
+        stack3.getChildren().addAll(pues, t3);
+        
+        Rectangle tur = new Rectangle(155,30);
+        tur.setFill(Color.rgb(102, 102, 255));
+        tur.setStroke(Color.rgb(102, 102, 255));
+        StackPane stack4 = new StackPane();
+        Text t4=new Text("TURNO");
+        t4.setFont(new Font(25));
+        t4.setFill(Color.WHITE);
+        stack4.getChildren().addAll(tur,t4);
+        
+        turnoPuesto.add(stack3, 0, 0);
+        turnoPuesto.add(stack4, 1, 0);
+        turnoPuesto.setHgap(0);
+        turnoPuesto.setVgap(1);
         for (Puesto p : AgenciaMigratoria.puestosDisponibles){
-            Label l=new Label("");
-            puestosTurnos.put(p.getId(), l);
-            turnoPuesto.add(l, 0, h);
+            Rectangle r = new Rectangle(155,30);
+            r.setFill(Color.rgb(102, 102, 255));
+            r.setStroke(Color.rgb(102, 102, 255));
+            StackPane stack = new StackPane();
+            Text t=new Text("");
+            t.setFont(new Font(25));
+            t.setFill(Color.WHITE);
+            stack.getChildren().addAll(r, t);
+            //Label l=new Label();
+            //l.setMinSize(4, 4);
+            //l.setFont(new Font(20));
+           // Label l2 = new Label(String.valueOf(p.getId()));
+            //l2.setStyle("-fx-background-color:POWDERBLUE");
+            Rectangle r2 = new Rectangle(155,30);
+            r2.setFill(Color.rgb(102, 178, 255));
+            r2.setStroke(Color.rgb(102, 178, 255));
+            StackPane stack2 = new StackPane();
+            Text t2=new Text(String.valueOf(p.getId()));
+            t2.setFont(new Font(25));
+            t2.setFill(Color.WHITE);
+            stack2.getChildren().addAll(r2, t2);
+            //l2.setFont(new Font(20));
+            puestosTurnos.put(p.getId(), t);
+            turnoPuesto.add(stack, 1, h);
+            turnoPuesto.add(stack2, 0, h);
             h++;
         }
+        
+        
     }
     public void llenarPuestos(){
         if(turnosEspera.size()<6){
@@ -220,8 +292,8 @@ public class FXMLTurnosController implements Initializable {
             }   
         }
         if(!turnosEspera.isEmpty()){
-            for(Map.Entry<Integer,Label> e: puestosTurnos.entrySet()){
-                Label l=e.getValue();
+            for(Map.Entry<Integer,Text> e: puestosTurnos.entrySet()){
+                Text l=e.getValue();
                 if(turnosEspera.peek()!= null && l.getText().equals("")){
                     Ticket t= turnosEspera.poll();
                     l.setText(t.getId());
